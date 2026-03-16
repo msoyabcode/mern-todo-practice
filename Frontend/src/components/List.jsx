82afb997d1a5ed0436951ba4306d70e8a3663bf9
msoyabcode <mohdsoyab0785@gmail.com>
2026-03-16 21:11:16 +0530

    Added delete task feature

--- Frontend/src/components/List.jsx
+++ Frontend/src/components/List.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 
 const List = () => {
 
@@ -15,24 +15,36 @@ const List = () => {
     getUiData()
   },[])
 
+
+  const deleteItem = async (id) =>{
+    let list = await fetch("http://localhost:3000/delete/"+id,{
+      method: "Delete"
+    })
+    list = await list.json() 
+   if(list){
+    getUiData()
+   }
+  }
   return (
     <div className=''>
       <h1 className='mt-9 flex justify-center items-center font-bold text-3xl underline'>List items</h1>
       
-        <ul className='grid grid-cols-3 gap-3 p-4 mt-12 '>
+        <ul className='grid grid-cols-4 gap-3 p-4 mt-12 '>
           <li className='bg-gray-100 shadow text-gray-600 rounded-md px-2 py-1 font-bold text-lg'>S.no.</li>
           <li className='bg-gray-100 shadow text-gray-600 rounded-md px-2 py-1 font-bold text-lg'>Titile</li>
           <li className='bg-gray-100 shadow text-gray-600 rounded-md px-2 py-1 font-bold text-lg'>Descripiton</li>
+          <li className='bg-gray-100 shadow text-gray-600 rounded-md px-2 py-1 font-bold text-lg'>Action</li>
 
           {uiData && uiData.map((item, index)=>(
-            <>
+            <Fragment key={item._id}>
              <li className='bg-gray-100 shadow text-gray-600 rounded-md px-2 py-1'>{index+1}</li>
              <li className='bg-gray-100 shadow text-gray-600 rounded-md px-2 py-1'>{item.title}</li>
              <li className='bg-gray-100 shadow text-gray-600 rounded-md px-2 py-1'>{item.description}</li>
-            </>
+             <li  className='bg-amber-400 hover:bg-amber-500 cursor-pointer shadow text-gray-600 rounded-md px-2 py-1'> <button className='cursor-pointer' onClick={()=>{deleteItem(item._id)}} >Delete</button></li>
+            </Fragment>
           ))}
+
         </ul>
-    
     </div>
   )
 }
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { collectionName, connection } from "./dbconfig.js";
 import cors from "cors";
+import { ObjectId } from "mongodb";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+
+// databse mein data store krne ke liye
 app.post("/add-data", async (req, res) => {
   const db = await connection();
   const collection = await db.collection(collectionName);
@@ -19,6 +22,8 @@ app.post("/add-data", async (req, res) => {
   }
 });
 
+
+// database mein jo data store hai usko ui mein dikhane ke liye
 app.get("/tasks", async (req, res) => {
   const db = await connection();
   const collection = db.collection(collectionName);
@@ -30,5 +35,21 @@ app.get("/tasks", async (req, res) => {
     res.send("your are fectching the API incorrectly");
   }
 });
+
+
+// koi item delete krne ke liye
+app.delete("/delete/:id", async (req, res)=>{
+  const db =  await connection()
+  const id = req.params.id
+  const collection = db.collection(collectionName)
+  let deleteItem = await collection.deleteOne({_id: new ObjectId(id)})
+  if(deleteItem){
+    res.send(deleteItem)
+  }else{
+    res.send("delte fetch is wrong")
+  }
+})
+
+
 
 app.listen(3000);
